3714987f3c41819e161ec3eb7ec90acd1fe914a1
Maimom <xmaimom@gmail.com>
2016-08-10 08:23:13 +0530

    New sample

---
+++ Soccer/Soccer/Soccer/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Soccer.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string City { get; set; }
+        public DateTime Founded { get; set; }
+        public virtual ICollection<Player> Players { get; set; } 
+    }
+}
